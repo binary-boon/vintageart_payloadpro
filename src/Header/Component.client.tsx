2d7e5aa10180b80acbc0059d275419d85c536336
binary-boon <the.binary.boon@gmail.com>
2025-09-18 18:29:48 +0530

    fixed logo colour inversion

--- src/Header/Component.client.tsx
+++ src/Header/Component.client.tsx
@@ -35,7 +35,7 @@ export const HeaderClient: React.FC<HeaderClientProps> = ({ data }) => {
     <header className="container relative z-20" {...(theme ? { 'data-theme': theme } : {})}>
       <div className="py-8 flex justify-between items-center">
         <Link href="/">
-          <Logo loading="eager" priority="high" className="invert dark:invert-0" />
+          <Logo loading="eager" priority="high" className="" />
         </Link>
 
         <div className="flex items-center gap-4">
